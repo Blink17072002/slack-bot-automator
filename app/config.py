71ee81992ed03932459455c436e35c2a7f6c5d95
Blink17072002 <adeyemojoshua17@gmail.com>
2026-04-03 12:41:55 +0100

    Update GitHub Actions cron time and clean up unused cron variables in config

--- app/config.py
+++ app/config.py
@@ -9,7 +9,4 @@ SLACK_CHANNEL = os.environ.get("SLACK_CHANNEL")
 
 DATABASE_URL = os.environ.get("DATABASE_URL") # postgres database
 REDIS_URL = os.environ.get("REDIS_URL")
-
-# Scheduler: Run by 8:00am every day
-CRON_HOUR = int(os.environ.get("CRON_HOUR", "8"))
-CRON_MINUTE = int(os.environ.get("CRON MINUTE", "0"))
+
